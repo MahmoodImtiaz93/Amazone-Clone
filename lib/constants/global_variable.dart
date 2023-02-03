b605ccecb4ffb36d098b0b07916a24cf9920217b
mahmood <mahmoodimtiaz.786@gmail.com>
2023-02-04 00:59:37 +0600

    Added Carousel_Images to home page

--- lib/constants/global_variable.dart
+++ lib/constants/global_variable.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-String uri ='http://10.0.2.2:3000';
+String uri = 'http://10.0.2.2:3000';
+
 class GlobalVariables {
   static const appBarGradient = LinearGradient(
     colors: [
@@ -47,7 +48,4 @@ class GlobalVariables {
       'image': 'assets/images/fashion.jpeg',
     },
   ];
-
-
-
 }
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazone_clone/constants/global_variable.dart';
 import 'package:amazone_clone/features/home/widgets/address_box.dart';
+import 'package:amazone_clone/features/home/widgets/carousel_image.dart';
 import 'package:amazone_clone/features/home/widgets/top_categories.dart';
 import 'package:amazone_clone/provider/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -92,6 +93,8 @@ class _HomeScreenState extends State<HomeScreen> {
           AddressBox(),
           SizedBox(height: 10),
           TopCategories(),
+          SizedBox(height: 10),
+          CarouselImage()
         ],
       ),
     );
